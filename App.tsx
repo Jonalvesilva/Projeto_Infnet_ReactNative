0c7d824bb595e7afea66ff7f36ed558bfea07728
Jonathan Silva <jonalves.silva@jsmail.com.br>
2023-04-03 12:42:17 -0300

    Alt App.tsx | Add TitleDescription | Alt Paragraph

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button, Alert, TextInput } from "react-native";
-import { Paragraph } from "./src/components/Paragraph";
+import { Paragraph, ParagraphMasters } from "./src/components/Paragraph";
+import {
+  TitleDescriptionBasic,
+  TitleDescriptionMasters,
+} from "./src/components/TitleDescription";
 
 export default function App() {
   return (
-    <View>
+    <View style={{ paddingTop: 40, paddingHorizontal: 5 }}>
+      <TitleDescriptionBasic title="Um titulo" description="Uma descrição" />
       <Text>Primeiro Teste do React Native</Text>
       <TextInput placeholder="Digite seu texto aqui"></TextInput>
       <Button
@@ -12,7 +17,7 @@ export default function App() {
         onPress={() => Alert.alert("olá")}
       ></Button>
       <View>
-        <Paragraph>Eu sou inevitável</Paragraph>
+        <ParagraphMasters>Eu sou inevitável</ParagraphMasters>
       </View>
     </View>
   );
--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, Text } from "react-native";
+import styled from "styled-components/native";
 
 type tipo = {
   children: string;
@@ -6,6 +7,13 @@ type tipo = {
 export function Paragraph(props) {
   return <Text style={styles.paragraphStyle}>{props.children}</Text>;
 }
+
+export const ParagraphMasters = styled.Text`
+  font-size: 18px;
+  line-height: 24px;
+  color: blue;
+  margin-vertical: 8px;
+`;
 
 const styles = StyleSheet.create({
   paragraphStyle: {
