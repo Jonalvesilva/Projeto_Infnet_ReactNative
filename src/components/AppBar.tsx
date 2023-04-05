dc7b0b0017f871abde260675d45e6665136a3514
Jonathan Silva <jonalves.silva@jsmail.com.br>
2023-04-05 10:17:37 -0300

    Add da AppBar.tsx

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,54 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+  TextInput,
+  Image,
+} from "react-native";
+import { Heading } from "./Heading";
+
+export function AppBar() {
+  return (
+    <View style={styles.appBar}>
+      <View>
+        <Image
+          style={styles.notepadImage}
+          source={require("../../assets/notepad.png")}
+        ></Image>
+      </View>
+      <View style={styles.viewHeader}>
+        <Heading title="Jon's Notes" header="h1"></Heading>
+      </View>
+      <View style={styles.viewButton}>
+        <Button title="Menu"></Button>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appBar: {
+    padding: 10,
+    flexDirection: "row",
+    gap: 20,
+    backgroundColor: "#111827",
+  },
+  notepadImage: {
+    width: 50,
+    height: 50,
+  },
+  fontTitle: {
+    fontFamily: "sans-serif",
+    color: "white",
+  },
+  viewHeader: {
+    flex: 2,
+  },
+  viewButton: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
