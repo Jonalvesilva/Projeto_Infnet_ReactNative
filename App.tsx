e5eb6757e353b6f4d11247f9b9c6deeb70a5d99e
Jonathan Silva <jonalves.silva@jsmail.com.br>
2023-04-05 13:27:40 -0300

    Add Navigation Container e Stack Navigator

--- App.tsx
+++ App.tsx
@@ -1,10 +1,41 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Home } from "./src/screens/Home";
+import { NotepadCreate } from "./src/screens/NotepadCreate";
+import { NotepadView } from "./src/screens/NotepadView";
+import { NotepadList } from "./src/screens/NotepadList";
+import { NotepadEdit } from "./src/screens/NotepadEdit";
+import screens from "./src/screens.json";
+import { AppBar } from "./src/components/AppBar";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Home />
+      <Stack.Navigator initialRouteName={screens.home}>
+        <Stack.Screen
+          name={screens.home}
+          component={Home}
+          options={{ navigationBarColor: "#111827", headerShown: false }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name={screens.notepadCreate}
+          component={NotepadCreate}
+        ></Stack.Screen>
+        <Stack.Screen
+          name={screens.notepadEdit}
+          component={NotepadEdit}
+        ></Stack.Screen>
+        <Stack.Screen
+          name={screens.notepadList}
+          component={NotepadList}
+        ></Stack.Screen>
+        <Stack.Screen
+          name={screens.notepadView}
+          component={NotepadView}
+        ></Stack.Screen>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
