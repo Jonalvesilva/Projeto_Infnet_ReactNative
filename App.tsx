ece630b26c796e498937be8bdd154da2edd0f0d0
Jonathan Silva <jonalves.silva@jsmail.com.br>
2023-04-06 14:51:45 -0300

    Alteracao na App.tsx

--- App.tsx
+++ App.tsx
@@ -8,17 +8,34 @@ import { NotepadList } from "./src/screens/NotepadList";
 import { NotepadEdit } from "./src/screens/NotepadEdit";
 import screens from "./src/screens.json";
 import { AppBar } from "./src/components/AppBar";
+import * as NavigationBar from "expo-navigation-bar";
 
 const Drawer = createDrawerNavigator();
+const screenOptionNavigation = {
+  swipeEnabled: false,
+  headerStyle: { backgroundColor: "#111827", height: 100 },
+  headerTitleStyle: { fontSize: 30 },
+  headerTintColor: "white",
+  drawerStyle: { backgroundColor: "lightgrey" },
+};
 
 export default function App() {
+  NavigationBar.setBackgroundColorAsync("#111827");
+
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName={screens.home}>
+      <Drawer.Navigator
+        initialRouteName={screens.home}
+        screenOptions={screenOptionNavigation}
+      >
         <Drawer.Screen
           name={screens.home}
           component={Home}
-          options={{ headerShown: false }}
+          options={{
+            headerRight: () => <AppBar></AppBar>,
+            headerTitle: "",
+            headerTitleStyle: { alignItems: "flex-end" },
+          }}
         ></Drawer.Screen>
         <Drawer.Screen
           name={screens.notepadCreate}
